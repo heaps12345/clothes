f7b2a7d7544e421992b0c1b5d73273782991cecb
Dustin heaps <dustinheaps89@gmail.com>
2019-07-17 23:19:59 -0400

    sign in

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
           });
         });
         // this.setState({ currentUser: user });
-        // console.log();
+        console.log();
       }
     });
   }
